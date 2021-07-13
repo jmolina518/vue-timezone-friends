198d53a868d5a1f8f3a07b098d11504e42f8382d
jmolina518 <jmolina518@gmail.com>
2021-07-13 01:34:37 -0500

    added profile image

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,13 +1,16 @@
 <template>
-  <ul>
-    <li v-for="user in users" :key="user.id">
-      {{ user.name }}
-    </li>
-  </ul>
+  <article class="person" v-for="user in users" :key="user.id">
+    <img :src="user.image" alt="" />
+    <div>
+      <h4>{{ user.name }}</h4>
+      <p>{{ user.timezone }}</p>
+    </div>
+  </article>
 </template>
 
 <script>
 import usersData from '../json/users.json';
+
 export default {
   data() {
     return {
@@ -18,8 +21,24 @@ export default {
 </script>
 
 <style scoped>
-div {
-  margin-top: 30px;
-  text-align: center;
+.person {
+  display: grid;
+  grid-template-columns: auto 1fr;
+  column-gap: 0.75rem;
+  margin-bottom: 1.5rem;
+  align-items: center;
+}
+.person img {
+  width: 75px;
+  height: 75px;
+  object-fit: cover;
+  border-radius: 50%;
+  box-shadow: var(--light-shadow);
+}
+.person h4 {
+  margin-bottom: 0.35rem;
+}
+.person p {
+  margin-bottom: 0;
 }
 </style>
