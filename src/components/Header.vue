fb2780422ffd278c3f420286c8ec2730c4e7b1a1
jmolina518 <jmolina518@gmail.com>
2021-07-12 23:40:48 -0500

    added a header

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <h1>
+      Timezone Friends
+    </h1>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+header {
+  margin-top: 30px;
+  text-align: center;
+}
+</style>
