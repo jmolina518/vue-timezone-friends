3d13ea7dcc88632f3d80db1c6c6b68c360053133
jmolina518 <jmolina518@gmail.com>
2021-07-15 00:05:21 -0500

    styled users component

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,14 +1,16 @@
 <template>
-  <article class="person" v-for="user in users" :key="user.id">
-    <a :href="user.twitter" target="_blank">
-      <img :src="user.image" alt="profile pic" />
-    </a>
-    <div>
-      <h4>{{ user.name }}</h4>
-      <p>{{ user.timezone }}</p>
-      <DateTime :timezone="user.timezone" class="date" />
-    </div>
-  </article>
+  <div class="container">
+    <article class="person" v-for="user in users" :key="user.id">
+      <a :href="user.twitter" target="_blank">
+        <img :src="user.image" alt="profile pic" />
+      </a>
+      <div>
+        <h4>{{ user.name }}</h4>
+        <p>{{ user.timezone }}</p>
+        <DateTime :timezone="user.timezone" class="date" />
+      </div>
+    </article>
+  </div>
 </template>
 
 <script>
@@ -28,19 +30,35 @@ export default {
 </script>
 
 <style scoped>
+.container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
 .person {
   display: grid;
   grid-template-columns: auto 1fr;
   column-gap: 0.75rem;
-  margin-bottom: 1.5rem;
   align-items: center;
+  width: 90vw;
+  background: #fff;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.4);
+  max-width: 300px;
+  margin: 30px auto;
+  margin-bottom: 1.5rem;
+  overflow: auto;
+  min-height: 100px;
+  border: 1px solid grey;
+  padding: 30px;
+  border-radius: 5px;
 }
 .person img {
   width: 75px;
   height: 75px;
   object-fit: cover;
   border-radius: 50%;
-  box-shadow: var(--light-shadow);
+  /* box-shadow: 0 5px 15px rgba(0, 0, 0, 0.1); */
 }
 .person h4 {
   margin-bottom: 0.35rem;
