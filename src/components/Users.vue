bbdb5084169ee8ffaf024970377e3c7f2a8d609b
jmolina518 <jmolina518@gmail.com>
2021-07-16 22:40:30 -0500

    added outer-container

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="container">
-    <article class="person" v-for="user in users" :key="user.id">
-      <a :href="user.twitter" target="_blank">
-        <img :src="user.image" alt="profile pic" />
-      </a>
-      <div class="info">
-        <h4>{{ user.name }}</h4>
-        <p>{{ user.timezone }}</p>
-        <DateTime :timezone="user.timezone" class="date" />
+  <div class="outer-container">
+    <div class="container">
+      <div class="card" v-for="user in users" :key="user.id">
+        <a :href="user.twitter" target="_blank" class="img-link">
+          <img :src="user.image" alt="profile pic" />
+        </a>
+        <div class="info">
+          <h4>{{ user.name }}</h4>
+          <p>{{ user.timezone }}</p>
+          <DateTime :timezone="user.timezone" class="date" />
+        </div>
       </div>
-    </article>
+    </div>
   </div>
 </template>
 
@@ -30,45 +32,60 @@ export default {
 </script>
 
 <style scoped>
+.outer-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  flex-wrap: wrap;
+}
 .container {
   display: flex;
   flex-direction: row;
+  /* justify-content: center; */
   flex-wrap: wrap;
-  align-content: center;
-  width: 60%;
+  width: 80vw;
+  max-width: 2000px;
   margin: 0 auto;
 }
 
-.person {
-  display: grid;
-  grid-template-columns: auto 1fr;
-  column-gap: 0.75rem;
+.card {
+  display: flex;
+  text-align: left;
+  column-gap: 1rem;
   align-items: center;
+  justify-content: space-around;
   width: 90vw;
   background: #fff;
   box-shadow: 0 5px 15px rgba(0, 0, 0, 0.4);
-  max-width: 300px;
-  margin: 2rem 1.5rem;
-  overflow: auto;
+  max-width: 350px;
+  margin: 1.5rem 1.5rem;
   min-height: 100px;
   border: 1px solid grey;
-  padding: 30px;
-  border-radius: 5px;
+  padding: 30px 15px;
+  border-radius: 7px;
 }
-.person img {
-  width: 75px;
-  height: 75px;
+
+.card img {
+  width: 90px;
+  height: 90px;
   object-fit: cover;
   border-radius: 50%;
+  align-self: center;
+  justify-self: center;
 }
-.person h4 {
+.card h4 {
   margin-bottom: 0.35rem;
 }
-.person p {
+.card p {
   margin-bottom: 0;
 }
 .info p,
 .date {
   color: grey;
+}
+.img-link {
+  margin: 0;
+  padding: 0;
+  max-height: 90px;
 }
 </style>
