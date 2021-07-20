b78b4db3250a51eb9c048cbedc6f9ade7b31eac0
jmolina518 <jmolina518@gmail.com>
2021-07-20 00:15:16 -0500

    add twitter link to name in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,9 @@
 <template>
   <footer>
-    <p>Made with <i class="fab fa-vuejs"></i> by Julio Molina</p>
+    <p>
+      Made with <i class="fab fa-vuejs"></i> by
+      <a href="https://twitter.com/julio_codes">Julio Molina</a>
+    </p>
     <p>
       Copyright &copy; 2021
     </p>
@@ -22,5 +25,19 @@ footer {
   text-align: center;
   color: aliceblue;
   font-size: 1.5rem;
+}
+a:link,
+:visited {
+  text-decoration: none;
+  color: aliceblue;
+}
+
+a:hover {
+  text-decoration: none;
+  color: rgba(59, 130, 246, 0.5);
+}
+
+a:active {
+  text-decoration: none;
 }
 </style>
